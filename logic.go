4cf2aff43b42d277ac82eed9a9367effa1cc3500
hillow <andrew.hillow@gmail.com>
2024-03-10 21:05:56 +0300

    feat: added add user logic

--- logic.go
+++ logic.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    "main/database"
+)
+
+func AddUser(user *tgbotapi.User) {
+    if user == nil {
+        return
+    }
+    err := DB.AddUser(database.User{Id: user.ID})
+    if err != nil {
+        ErrorLog.Printf("Failed to add user %v\n", err)
+        return
+    }
+    InfoLog.Printf("Added user %v (%v)\n", user.UserName, user.ID)
+}
