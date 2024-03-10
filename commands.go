e26041ebdec84e28d78246345d2c197f4f3fa771
hillow <andrew.hillow@gmail.com>
2024-03-10 17:59:27 +0300

    feat: added start command

--- commands.go
+++ commands.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func startCommand(update tgbotapi.Update) {
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello world")
+    _, err := bot.Send(msg)
+    if err != nil {
+        ErrorLog.Printf("Start command: %v\n", err)
+        return
+    }
+    InfoLog.Printf("Start command send to %v\n", update.Message.From.UserName)
+}
