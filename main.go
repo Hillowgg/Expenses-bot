c06563590c6601a5fee7df95b5581211459e8c2e
hillow <andrew.hillow@gmail.com>
2024-03-10 17:28:49 +0300

    feat: created bot and polling in main.go

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "os"
+    "strconv"
+
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+func resolveUpdate(update tgbotapi.Update) {
+
+}
+
+// todo: create config loader?
 func main() {
-    fmt.Println("hello world")
+    bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+    if err != nil {
+        log.Fatalf("[FATAL] %v\n", err)
+    }
+    log.Printf("[INFO] Logged in as %v\n", bot.Self.UserName)
+
+    updateConfig := tgbotapi.NewUpdate(0)
+    timeout, err := strconv.Atoi(os.Getenv("TIMEOUT"))
+    updateConfig.Timeout = timeout
+
+    updateChan := bot.GetUpdatesChan(updateConfig)
+    log.Println("[INFO] Started polling")
+    for update := range updateChan {
+        go resolveUpdate(update)
+    }
 }
