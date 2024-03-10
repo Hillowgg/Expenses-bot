d6a39b9b4043b14846f669f2835c94769af7d529
hillow <andrew.hillow@gmail.com>
2024-03-10 17:58:54 +0300

    feat: set up logging

--- main.go
+++ main.go
@@ -19,22 +19,36 @@ func resolveUpdate(update tgbotapi.Update) {
 }
 
 var bot *tgbotapi.BotAPI
+var (
+    InfoLog  *log.Logger
+    WarnLog  *log.Logger
+    ErrorLog *log.Logger
+    FatalLog *log.Logger
+)
+
+func init() {
+    flags := log.LstdFlags | log.Lshortfile
+    InfoLog = log.New(os.Stdout, "[INFO] ", flags)
+    WarnLog = log.New(os.Stdout, "[WARN] ", flags)
+    ErrorLog = log.New(os.Stderr, "[ERROR] ", flags)
+    FatalLog = log.New(os.Stderr, "[FATAL] ", flags)
+}
 
 // todo: create config loader?
 func main() {
     var err error
     bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
     if err != nil {
-        log.Fatalf("[FATAL] %v\n", err)
+        FatalLog.Fatalln(err)
     }
-    log.Printf("[INFO] Logged in as %v\n", bot.Self.UserName)
+    InfoLog.Printf("Logged in as %v\n", bot.Self.UserName)
 
     updateConfig := tgbotapi.NewUpdate(0)
     timeout, err := strconv.Atoi(os.Getenv("TIMEOUT"))
     updateConfig.Timeout = timeout
 
     updateChan := bot.GetUpdatesChan(updateConfig)
-    log.Println("[INFO] Started polling")
+    InfoLog.Println("Started polling")
     for update := range updateChan {
         go resolveUpdate(update)
     }
