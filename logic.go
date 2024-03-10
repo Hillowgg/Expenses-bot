38d193decb8a074ff57c63de5c9c859535f2cd9e
hillow <andrew.hillow@gmail.com>
2024-03-10 22:09:23 +0300

    feat: addred return error to AddUser

--- logic.go
+++ logic.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+    "errors"
+
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
     "main/database"
 )
 
-func AddUser(user *tgbotapi.User) {
+func AddUser(user *tgbotapi.User) error {
     if user == nil {
-        return
+        return errors.New("nil user")
     }
     s, err := DB.AddUser(database.User{Id: user.ID})
     if err != nil {
         ErrorLog.Printf("Failed to add user: %v\n", err)
-        return
+        return err
     }
     if s {
         InfoLog.Printf("Added user %v (%v)\n", user.UserName, user.ID)
     }
+    return nil
 }
